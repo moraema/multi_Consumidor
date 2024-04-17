37f5d92b406a25ad4e69ebb1f66617796e851afc
emma <223729@ids.upchiapas.edu.mx>
2024-04-16 20:13:56 -0600

    se arreglo el envio de datos

--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,25 @@ const initBroker = async () => {
         let connection = await amqplib.connect('amqp://3.229.81.220');
         let ch = await connection.createChannel();
     
-       
-        ch.consume('temperatura', async (msg: any) => {
+        ch.consume('calidad', async (msg: any) => {
             try {
-                const datos_temperatura = JSON.parse(msg.content.toString());
-                console.log('Datos de temperatura:', datos_temperatura);
-                
-              
-                await enviarDatosAlServidor(datos_temperatura);
+                // Deserializar el mensaje JSON recibido del broker
+                const datos = JSON.parse(msg.content.toString());
+                console.log('Datos recibidos:', datos);
+
+                // Extraer los valores de temperatura, luz y humedad
+                const { temperatura, luz, humedad } = datos;
+
+                // Enviar los datos a la API REST como un objeto con los atributos separados
+                const response = await axios.post('http://localhost:3000/racimos', { 
+                    temperatura,
+                    luz,
+                    humedad
+                });
+
+                console.log('Respuesta de la API:', response.data);
             } catch (error) {
-                console.error('Error al enviar los datos de temperatura:', error);
-            }
-
-            ch.ack(msg);
-        });
-
-       
-        ch.consume('humedad', async (msg: any) => {
-            try {
-                const datos_humedad = JSON.parse(msg.content.toString());
-                console.log('Datos de humedad:', datos_humedad);
-                
-                
-                await enviarDatosAlServidor(datos_humedad);
-            } catch (error) {
-                console.error('Error al enviar los datos de humedad:', error);
-            }
-
-            ch.ack(msg);
-        });
-
-       
-        ch.consume('luz', async (msg: any) => {
-            try {
-                const datos_luz = JSON.parse(msg.content.toString());
-                console.log('Datos de luz:', datos_luz);
-                
-                
-                await enviarDatosAlServidor(datos_luz);
-            } catch (error) {
-                console.error('Error al enviar los datos de luz:', error);
+                console.error('Error al enviar los datos a la API:', error);
             }
 
             ch.ack(msg);
@@ -54,21 +33,6 @@ const initBroker = async () => {
         console.log('El broker ha iniciado correctamente');
     } catch (error) {
         console.log('Hubo un error al iniciar el broker', error);
-    }
-}
-
-const enviarDatosAlServidor = async (datos: any) => {
-    try {
-        const { temperatura, luz, humedad } = datos;
-        const data = {
-            temperatura,
-            luz,
-            humedad
-        };
-        const response = await axios.post('http://localhost:3000/racimos', data);
-        console.log('Respuesta de la API:', response.data);
-    } catch (error) {
-        console.error('Error al enviar los datos a la API:', error);
     }
 }
 
